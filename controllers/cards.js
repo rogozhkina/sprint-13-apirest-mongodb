b3883f82250e43fb7f63db4e4ec7e3e5a7659632
taschunina <taschunina@yandex.ru>
2020-08-18 21:59:11 +0300

    Контроллеры для пользователя и карточки

---
+++ controllers/cards.js
@@ -0,0 +1,22 @@
+const Card = require('../models/card');
+
+module.exports.getCards = (req, res) => {
+  Card.find({})
+  .populate('user')
+    .then(cards => res.send({ data: cards}))
+    .catch(err => res.status(500).send({ message: 'Произошла ошибка'}));
+}
+
+module.exports.createCard = (req, res) => {
+  const { name, link } = req.body;
+  const userId = req.user._id;
+  Card.create({ name, link, owner: userId  })
+    .then(card => res.send({ data: card}))
+    .catch(err => res.status(500).send({ message: 'Произошла ошибка'}));
+}
+
+module.exports.deleteCardById = (req, res) => {
+  Card.findByIdAndRemove(req.params.id)
+    .then(card => res.send({ data: card}))
+    .catch(err => res.status(500).send({ message: 'Произошла ошибка'}));
+}
