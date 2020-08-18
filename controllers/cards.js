e0953ceef79e9340d7733a06edc4f6bdc540fa29
taschunina <taschunina@yandex.ru>
2020-08-18 23:09:17 +0300

    Исправлен текст сообщения об ошибке в cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -4,7 +4,7 @@ module.exports.getCards = (req, res) => {
   Card.find({})
   .populate('user')
     .then(cards => res.send({ data: cards}))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка'}));
+    .catch(err => res.status(500).send({ message: 'На сервере произошла ошибка'}));
 }
 
 module.exports.createCard = (req, res) => {
@@ -12,11 +12,11 @@ module.exports.createCard = (req, res) => {
   const userId = req.user._id;
   Card.create({ name, link, owner: userId  })
     .then(card => res.send({ data: card}))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка'}));
+    .catch(err => res.status(500).send({ message: 'На сервере произошла ошибка'}));
 }
 
 module.exports.deleteCardById = (req, res) => {
   Card.findByIdAndRemove(req.params.id)
     .then(card => res.send({ data: card}))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка'}));
+    .catch(err => res.status(500).send({ message: 'На сервере произошла ошибка'}));
 }
